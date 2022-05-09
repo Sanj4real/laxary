6f09f5e5ecc324f77177c08611c6ba25c392545c
Sanjay Rawal <rsanj054@gmail.com>
2022-05-09 13:47:56 +0545

    laxarry

---
+++ luxury/homepage/payment-sec.php
@@ -0,0 +1,30 @@
+<section class="payment">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="payment-block payment--img">
+                    <img src="
+                    
+                    <?php
+                    echo $image
+                     ?>
+                    
+                    " alt="Dynamic Image">
+                    
+                    <img src="image.php?image=chair" alt="">
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="payment-block">
+                    <div class="pay-content">
+                        <h4><?php echo $title ?></h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut
+                            labore et dolore magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+</section>
